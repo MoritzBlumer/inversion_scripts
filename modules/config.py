fb828596a69004a812d545a8dfbbf8cadcc6d790
MoritzBlumer <50796708+MoritzBlumer@users.noreply.github.com>
2023-04-17 14:29:27 -0600

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,10 +1,10 @@
-## SETINGS (ok to change)
+## GLOBAL SETINGS (ok to change)
 min_var_per_w = 25  # minimum number of polymorphic variants per window
 mean_threshold = 3  # if no guide samples specified, use $mean_threshold samples with largest mean 
                     # to select guide samples
 float_precision = 7 # float precision for text output
 
 
-## SHARED VARIABLES (don't change)
+## SHARED VARIABLES (don't change anything below here, min_maf can be specified in the command line)
 n_windows = None
 min_maf = None
