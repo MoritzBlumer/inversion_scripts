8306491148230ee50fc5e12ada83e09789826bf3
Moritz Blumer <moritzblumer@wl-164-205.wireless.umt.edu>
2023-04-17 14:19:32 -0600

    added config file

---
+++ modules/config.py
@@ -0,0 +1,11 @@
+
+## SETINGS (ok to change)
+min_var_per_w = 25  # minimum number of polymorphic variants per window
+mean_threshold = 3  # if no guide samples specified, use $mean_threshold samples with largest mean 
+                    # to select guide samples
+float_precision = 7 # float precision for text output
+
+
+## SHARED VARIABLES (don't change)
+n_windows = None
+min_maf = None
